a129387be3bbbe0762c203a443c60eda89537e74
Pransquare <chagamobulesu@pransquare.in>
2025-09-03 14:20:58 +0530

    creating feature branch

--- src/main/java/com/pransquare/dashboard/MasterConfigApplication.java
+++ src/main/java/com/pransquare/dashboard/MasterConfigApplication.java
@@ -1,4 +1,4 @@
-package com.pransqaure.dashboard;
+package com.pransquare.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/pransquare/dashboard/ServletInitializer.java
+++ src/main/java/com/pransquare/dashboard/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.pransqaure.dashboard;
+package com.pransquare.dashboard;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
